f85af7d5d21c718eb541caad81c13c9bcbbfa776
Yusuf Hilmi <yusufhilmicr@gmail.com>
2020-08-11 23:24:53 +0300

    finished

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // AUTHOR: YUSUF HILMI
 // 
+
+
+/////////////////////////////////////////////////////
+///////////  THINGS YOU CAN CHANGE //////////////////
+
+
+// CHANGE THIS UNTIL URE STAISFIED WITH THE COMPONENT SIZES
+const SCALE = 3.5;
+
+// CHOOSE ELEMENTS WITH ID "thing<number>"  
+const bigs = [15, 6, 2, 3, 4, 17, 13, 14, 12, 16];  // INSERT ID OF BIG ELEMENTS TO HERE 
+
+const NUMBEROFBIGS = 5;  // HOW MANY BIG SHOULD BE INSIDE FRAME
+const dotnumbers = randomize(10, 50);  // RANDOM NUMBER OF DOTS
+
+const SMALLELEMENTSRANDOM = 3;   // SMALL ELEMENTS CAN APPEAR +1
+
+
+///////////  THIS PARAMETERS SHOULD SUFFICE /////////
+/////////////////////////////////////////////////////
+
+
 function getFrame() {
     var frame_pos = document.querySelector("#frame").getBoundingClientRect();
     return frame_pos
@@ -8,20 +30,40 @@ function getFrame() {
 function randomize(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 var frame_pos = getFrame();
 
-
 var dot = d3.select("#dot");
-dot.attr("width", 20); // WIDTH OF DOT
+dot.attr("transform", `scale(${SCALE})`);
 dot.attr("style", `top: ${randomize(frame_pos.top, frame_pos.bottom)}; left: ${randomize(frame_pos.right, frame_pos.left)}`);
 
-for (var i = 2; i < 19; i++) {
-    let thing = d3.select(`#thing${i}`);
-    console.log(`thing${i}`)
+var chosenBigs = [];
+for (let i = 0; i < NUMBEROFBIGS; i++) {
+    let randomized_index = randomize(0, bigs.length);
+    console.log(randomized_index);
+    chosenBigs.push(randomized_index);
+}
 
-    width = randomize(50, frame_pos.width / 4);  // RANDOM WIDTH PARAMETERS FOR ALL
-    height = randomize(50, frame_pos.height / 4); // RANDOM HEIGHT PARAMETERS FOR ALL
+console.log(chosenBigs);
+
+for (var i = 2; i < 17; i++) {
+
+    if ((bigs.includes(i)) && !chosenBigs.includes(i)) {
+        console.log(i);
+        continue;
+    }
+    if(randomize(0,1)===0 && !(bigs.includes(i))){
+        continue;
+    }
+
+    let thing = d3.select(`#thing${i}`);
+    // console.log(`thing${i}`);
+    // console.log(randomize(0,1));
+
+
+    thing.attr("transform", `scale(${SCALE})`)
+    component_frame = document.querySelector(`#thing${i}`).getBoundingClientRect();
+    width = component_frame.width;  // RANDOM WIDTH PARAMETERS FOR ALL
+    height = component_frame.height; // RANDOM HEIGHT PARAMETERS FOR ALL
     rotation = randomize(0, 360);  // RANDOM ROTATION
 
     thing.attr("width", width);
@@ -31,28 +73,22 @@ for (var i = 2; i < 19; i++) {
     thing.attr("style", `top: ${randomize(frame_pos.top, frame_pos.bottom - height)}; left: ${randomize(frame_pos.left, frame_pos.right - width)}`);
     thing.attr("transform", ` rotate(${rotation})`);
 
+    if (!(bigs.includes(i))) {
+        loopoverloop = randomize(0, SMALLELEMENTSRANDOM);   // RANDOM NUMBER OF OTHER ELEMENTS !there is already one created before!
+        for (var j = 0; j < loopoverloop; j++) {
+            svgrotation = randomize(0, 360);
 
-    loopoverloop = randomize(0, 2);   // RANDOM NUMBER OF OTHER ELEMENTS !there is already one created before!
-    for (var j = 0; j < loopoverloop; j++) {
-        svgwidth = randomize(50, frame_pos.width / 4);  // RANDOM WIDTH PARAMETERS FOR ALL
-        svgheight = randomize(50, frame_pos.height / 4); // RANDOM HEIGHT PARAMETERS FOR ALL
-        svgrotation = randomize(0, 360);
-
-        $(`#thing${i}`).clone()
-            .attr("width", svgwidth)
-            .attr("height", svgheight)
-            .attr("style", `top: ${randomize(frame_pos.top, frame_pos.bottom - svgheight)}; left: ${randomize(frame_pos.left, frame_pos.right - svgwidth)}`)
-            .attr("transform", ` rotate(${svgrotation})`)
-            .appendTo(".container");
+            $(`#thing${i}`).clone()
+                .attr("style", `top: ${randomize(frame_pos.top, frame_pos.bottom - height)}; left: ${randomize(frame_pos.left, frame_pos.right - width)}`)
+                .attr("transform", ` rotate(${svgrotation})`)
+                .appendTo(".container");
+        }
     }
 
 }
 
-
-var dotnumbers = randomize(0, 30);  // RANDOM NUMBER OF DOTS
 for (var i = 0; i < dotnumbers; i++) {
     $("#dot").clone()
-        .attr("style", `top: ${randomize(frame_pos.top, frame_pos.bottom - height)}; left: ${randomize(frame_pos.left, frame_pos.right - width)}`)
+        .attr("style", `top: ${randomize(frame_pos.top, frame_pos.bottom)}; left: ${randomize(frame_pos.left, frame_pos.right)}`)
         .appendTo(".container");
 }
-
